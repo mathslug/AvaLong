0599cb8f4dc5f832253a6da02e8bc7cf3883a6a8
mathslug <theheadslug@mathslug.com>
2023-12-04 01:32:12 -0500

    log

--- AvalonGame.py
+++ AvalonGame.py
@@ -55,18 +55,32 @@ class AvalonGame:
         self.mission_actions = {}
         self.mechanic_mode = "proposal"
         self.winner = ""
+        self.log = []
+
+    def get_game_params(self):
+        return {
+            "turn order:": self.turn_order,
+            "mission_participants": self.mission_participants,
+            "fails_required": self.fails_required,
+            "characters": self.characters
+        }
 
     def get_game_state(self):
-        # TODO make this more dynamic based on mechanic_mode, to create a log
         return {
-            "current_turn": self.turn_order[self.current_turn],
-            "turn order:": self.turn_order,
             "completed_missions": self.completed_missions,
             "consecutive_rejects": self.consecutive_rejects,
-            "mission_participants": self.mission_participants[len(self.completed_missions)],
-            "fails_required": self.fails_required[len(self.completed_missions)],
-            "proposed_team": self.proposed_team,
-            "current_mechanic_mode": self.mechanic_mode
+            "current_turn": self.turn_order[self.current_turn],
+            "current_mechanic_mode": self.mechanic_mode,
+            "proposed_team": self.proposed_team
+        }
+    
+    def get_game_results(self):
+        if self.mechanic_mode != "ended":
+            raise ValueError("Game must have ended to see results.")
+        return {
+            "winner": self.winner,
+            "player_characters": self.player_characters,
+            "log": self.log
         }
 
     def get_player_known_info(self, player_name):
@@ -90,7 +104,7 @@ class AvalonGame:
         return info
     
     def propose_team(self, player_name, team):
-        if not self.mechanic_mode == "proposal":
+        if self.mechanic_mode != "proposal":
             raise ValueError("It is not time for mission proposal.")
         
         if player_name != self.turn_order[self.current_turn]:
@@ -104,9 +118,10 @@ class AvalonGame:
 
         self.proposed_team = team
         self.mechanic_mode = "voting"
+        self.log.append(f"{player_name} proposed {team}")
     
     def player_vote(self, player_name, vote):
-        if not self.mechanic_mode == "voting":
+        if self.mechanic_mode != "voting":
             raise ValueError("It is not time for voting on the proposed mission.")
 
         if player_name not in self.player_characters:
@@ -121,20 +136,23 @@ class AvalonGame:
 
             # Check if the vote fails
             if true_votes <= len(self.player_characters) / 2:
+                self.log.append(f"rejected by {[k for (k, v) in votes.items() if not v]}")
                 self.consecutive_rejects += 1
                 self.proposed_team = []
                 self.votes = {}
                 if self.consecutive_rejects == 5:
                     self.winner = "evil"
                     self.mechanic_mode = "ended"
+                    self.log.append("Evil team wins via rejections.")
                 else:
                     self.current_turn = (self.current_turn + 1) % len(self.player_characters)
                     self.mechanic_mode = "proposal"
             else:
+                self.log.append(f"approved by {[k for (k, v) in votes.items() if v]}")
                 self.mechanic_mode = "mission"
 
     def player_mission_act(self, player_name, succeed_mission):
-        if not self.mechanic_mode == "mission":
+        if self.mechanic_mode != "mission":
             raise ValueError("It is not time to go on a mission.")
 
         if player_name not in self.proposed_team:
@@ -152,6 +170,7 @@ class AvalonGame:
             mission_fail_count = sum(not action for action in self.mission_actions.values())
             mission_result = mission_fail_count < self.fails_required[len(self.completed_missions)]
             self.completed_missions.append(mission_result)
+            self.log.append(f"mission {'passed' if mission_result else 'failed'} with {mission_fail_count} fails")
 
             # Reset states for the next mission
             self.proposed_team = []
@@ -161,6 +180,7 @@ class AvalonGame:
             if sum([not m for m in self.completed_missions]) >= 3:
                 self.winner = "evil"
                 self.mechanic_mode = "ended"
+                self.log.append("Evil team wins via missions")
             elif sum(self.completed_missions) >= 3:
                 self.mechanic_mode = "assassination"
             else:
@@ -168,7 +188,7 @@ class AvalonGame:
                 self.mechanic_mode = "proposal"
         
     def assassination(self, player_name, target):
-        if not self.mechanic_mode == "assassination":
+        if self.mechanic_mode != "assassination":
             raise ValueError("It is not time for assassination.")
 
         if self.player_characters[player_name] != "Assassin":
@@ -179,6 +199,12 @@ class AvalonGame:
         
         if self.player_characters[target] == "Merlin":
             self.winner = "evil"
+            self.log.append("Evil team wins via assassination")
         else:
             self.winner = "good"
+            self.log.append("Good team wins")
         self.mechanic_mode = "ended"
+
+g = AvalonGame(["a", "b", "c", "d", "e"])
+print(g.get_game_params())
+print(g.get_game_state())
